19e73c751dc8b707a0a78d389284494401f8b7a9
AlterJuice <alterjuice1758@gmail.com>
2022-06-27 19:36:22 +0300

    Created Setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='JuicyUtils',
+    version='1.0',
+    packages=['sshtunnel', "sqlite3", "time"],
+    url='github.com/AlterJuice',
+    license='',
+    author='AlterJuice',
+    author_email='alterjuice1758@gmail.com',
+    description='Simple DBCLiteServer'
+)
